86f183532a8552d09c69ae4fc9c97409aab70e59
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-03-03 16:52:58 -0600

    Player can't click same square twice

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 let gameBoard = (arg) => {
-    let square = document.querySelectorAll(".square")
+    let turnHistory = [];
+    let square = document.querySelectorAll(".square");
     let playerChoice = () => {
-        square.forEach((el) => {
+        square.forEach((el, i) => {
             el.addEventListener("click", (e) => {
+                turnHistory.push(i);
+                // To avoid clicking the same square twice
+                el.classList.add("disableClick")
                 el.textContent = "X"
                 compChoice()
             })
-        });
-    }
-
+        })
+    };
     let compChoice = () => {
-        let randomNum = Math.floor(Math.random() * 6);
-        square[randomNum].textContent = "O"
-        // console.log(randomNum);
-    }
-
+        let randomNum = Math.floor(Math.random() * 9);
+        if (turnHistory.length <= 8) {
+            if (!turnHistory.includes(randomNum)) {
+                turnHistory.push(randomNum)
+                square[randomNum].classList.add("disableClick")
+                square[randomNum].textContent = "O"
+            } else {
+                compChoice()
+            }
+        }
+    };
     return { playerChoice }
 }
-
-
 
 let game = gameBoard()
 
@@ -29,7 +36,24 @@ game.playerChoice()
 
 
 
+// if (num.length === 0) {
+//     console.log(num);
+//     num.push(randomNum)
+//     console.log(num.length);
+// }
 
+// else {
+//     console.log(num);
+//     let lastNum = num[num.length - 1];
+//     num.map(el => {
+//         if (el !== randomNum) {
+//             num.push(randomNum)
+//         }
+
+//     })
+// }
+
+// square[lastNum].textContent = "O"
 // console.log(square);
         // arg.textContent = "O"
         // for (i = 0; i < square.length; i++) {
