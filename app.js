898db3f7713fabd2941b533e8a7f81d3ecbccc29
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-03-03 08:16:36 -0600

    Comp can play now

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-let square = document.getElementsByClassName("square");
-
-for (i = 0; i < square.length; i++) {
-    square[i].onclick = (e) => {
-        e.path[0].innerHTML = "X"
+let gameBoard = (arg) => {
+    let square = document.querySelectorAll(".square")
+    let playerChoice = () => {
+        square.forEach((el) => {
+            el.addEventListener("click", (e) => {
+                el.textContent = "X"
+                compChoice()
+            })
+        });
     }
+
+    let compChoice = () => {
+        let randomNum = Math.floor(Math.random() * 6);
+        square[randomNum].textContent = "O"
+        // console.log(randomNum);
+    }
+
+    return { playerChoice }
 }
 
+
+
+let game = gameBoard()
+
+game.playerChoice()
+
+
+
+
+
+
+
+// console.log(square);
+        // arg.textContent = "O"
+        // for (i = 0; i < square.length; i++) {
+        //     square[randomNum].textContent = "asdas"
+        // }
+        // let test = document.querySelector(".square:nth-child(1)")
+        // console.log(randomNum);
