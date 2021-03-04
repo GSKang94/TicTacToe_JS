07c48dc708b9f02a28e69b3d15ededdeabbebbfe
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-03-03 20:27:07 -0600

    Added checkWinner logic

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-let gameBoard = (arg) => {
+let gameBoard = () => {
     let turnHistory = [];
+    let playerHistory = [];
+    let compHistory = [];
     let square = document.querySelectorAll(".square");
+
     let playerChoice = () => {
         square.forEach((el, i) => {
-            el.addEventListener("click", (e) => {
+            el.addEventListener("click", () => {
                 turnHistory.push(i);
                 // To avoid clicking the same square twice
                 el.classList.add("disableClick")
                 el.textContent = "X"
+                playerHistory.push(+el.id);
+
                 compChoice()
             })
         })
@@ -19,23 +24,65 @@ let gameBoard = (arg) => {
                 turnHistory.push(randomNum)
                 square[randomNum].classList.add("disableClick")
                 square[randomNum].textContent = "O"
+                compHistory.push(+square[randomNum].id);
+                // console.log("comp", compHistory);
             } else {
                 compChoice()
             }
         }
+        checkWinner()
     };
-    return { playerChoice }
+    return { playerChoice, playerHistory, compHistory }
 }
 
 let game = gameBoard()
 
 game.playerChoice()
 
+let checkWinner = () => {
+    let [p1, p2, p3, p4, p5] = game.playerHistory;
+    let [c1, c2, c3, c4] = game.compHistory;
+    // console.log(game.compHistory);
+    if ((p1 + p2 + p3) === 15) {
+        console.log('You win!');
+    } else if ((p2 + p3 + p4) === 15) {
+        console.log('You Win!!');
+    } else if ((p3 + p4 + p5) === 15) {
+        console.log('You Win');
+    } else if ((c1 + c2 + c3) === 15) {
+        console.log('Comp Win');
+    } else if ((c2 + c3 + c4) === 15) {
+        console.log('Comp Win!');
+    }
+}
 
 
 
 
 
+// let winner = () => {
+//     let winnerScore = 15;
+//     let [a, , b, , c, , d, , e] = turnHistory;
+    // switch (winnerScore) {
+    //     case a + b + c:
+    //         console.log('You won');
+    //         break
+    //     case b + c + d:
+    //         console.log('You won!');
+    //         break
+    //     case c + d + e:
+    //         console.log('You Won!!!');
+    //         break
+    //     default:
+    //         console.log('Game is still going');
+    //         break
+    // }
+    // if ((a + b + c) = winnerScore) {
+    //     console.log('You won');
+    // }
+//     console.log(a, b, c);
+//     console.log(a + b + c);
+// }
 // if (num.length === 0) {
 //     console.log(num);
 //     num.push(randomNum)
